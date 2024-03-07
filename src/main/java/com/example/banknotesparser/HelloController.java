5cacf91061446ab6f65fd8d6d283aa2f4751315c
pashtetrus33 <testerrus33@gmail.com>
2024-03-07 15:02:37 +0300

    pretty fix

--- src/main/java/com/example/banknotesparser/HelloController.java
+++ src/main/java/com/example/banknotesparser/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.banknotesparser;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HelloController {
 
@@ -18,6 +20,13 @@ public class HelloController {
     @FXML
     private TextArea counterField;
 
+    @FXML
+    private TextArea expectedField;
+    @FXML
+    private TextArea actualField;
+    @FXML
+    private TextArea testResultField;
+
     int counter50 = 0;
     int counter20 = 0;
     int counter10 = 0;
@@ -27,79 +36,113 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
 
-        int counter50 = 0;
-        int counter20 = 0;
-        int counter10 = 0;
-        int counter5 = 0;
-        int counter1 = 0;
+        AtomicInteger counter50 = new AtomicInteger();
+        AtomicInteger counter20 = new AtomicInteger();
+        AtomicInteger counter10 = new AtomicInteger();
+        AtomicInteger counter5 = new AtomicInteger();
+        AtomicInteger counter1 = new AtomicInteger();
 
         StringBuilder stringBuilder = new StringBuilder();
         List<Integer> integerList;
 
         try {
+            testResultField.setStyle("");
+            actualField.clear();
+            expectedField.clear();
+            outputField.clear();
+            counterField.clear();
 
             integerList = Arrays.asList(inputField.getText().split(",")).stream()
                     .map(Integer::parseInt)
                     .toList();
-            for (Integer num : integerList) {
 
-                if (num >= 100 || num <= 0) {
-                    stringBuilder.append("Некорректное значение. Требование: больше нуля и меньше 100!\n");
-                    continue;
+            int expected = integerList.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+
+            expectedField.setText("Ожидается $: " + expected);
+
+            Thread thread = new Thread(() -> {
+                for (Integer num : integerList) {
+
+                    if (num >= 100 || num <= 0) {
+                        stringBuilder.append("Некорректное значение. Требование: больше нуля и меньше 100!\n");
+                        continue;
+                    }
+                    if (num >= 10) {
+                        stringBuilder.append("Число: $").append(num).append("  ->  ");
+                    } else {
+                        stringBuilder.append("Число: $").append(num).append("    ->  ");
+                    }
+
+                    if (num >= 50) {
+                        stringBuilder.append("$50: 1  ");
+                        counter50.getAndIncrement();
+                        num -= 50;
+                    } else {
+                        stringBuilder.append("$50: 0  ");
+                    }
+                    if (num >= 40) {
+                        stringBuilder.append("$20: 2  ");
+                        counter20.addAndGet(2);
+                        num -= 40;
+                    } else if (num >= 20) {
+                        stringBuilder.append("$20: 1  ");
+                        counter20.getAndIncrement();
+                        num -= 20;
+
+                    } else {
+                        stringBuilder.append("$20: 0  ");
+                    }
+
+                    if (num >= 10) {
+                        stringBuilder.append("$10: 1  ");
+                        counter10.getAndIncrement();
+                        num -= 10;
+                    } else {
+                        stringBuilder.append("$10: 0  ");
+                    }
+
+                    if (num >= 5) {
+                        stringBuilder.append("$5: 1  ");
+                        counter5.getAndIncrement();
+                        num -= 5;
+                    } else {
+                        stringBuilder.append("$5: 0  ");
+                    }
+                    stringBuilder.append("$1: ").append(num).append("\n");
+                    counter1.addAndGet(num);
+
+                    Platform.runLater(() -> outputField.setText(stringBuilder.toString()));
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-                if (num >= 10){
-                    stringBuilder.append("Число: $").append(num).append("  ->  ");
+
+                int actual = counter50.get() * 50 + counter20.get() * 20 +
+                        counter10.get() * 10 + counter5.get() * 5 + counter1.get();
+                actualField.setText("Итого $: " + actual);
+
+                if (expected == actual) {
+                    testResultField.setStyle("-fx-control-inner-background: #00FF00;");
                 } else {
-                    stringBuilder.append("Число: $").append(num).append("    ->  ");
+                    testResultField.setStyle("-fx-control-inner-background: #FF0000;");
                 }
 
-                if (num >= 50) {
-                    stringBuilder.append("$50: 1  ");
-                    counter50++;
-                    num -= 50;
-                } else {
-                    stringBuilder.append("$50: 0  ");
-                }
-                if (num >= 40) {
-                    stringBuilder.append("$20: 2  ");
-                    counter20 += 2;
-                    num -= 40;
-                } else if (num >=20){
-                    stringBuilder.append("$20: 1  ");
-                    counter20++;
-                    num -= 20;
+                counterField.setText("$50: " + counter50 + "\n" + "$20: " + counter20 + "\n" + "$10: " + counter10 + "\n"
+                        + "$5: " + counter5 + "\n" + "$1: " + counter1 + "\n");
+            });
 
-                } else {
-                    stringBuilder.append("$20: 0  ");
-                }
-
-                if (num >= 10) {
-                    stringBuilder.append("$10: 1  ");
-                    counter10++;
-                    num -= 10;
-                } else {
-                    stringBuilder.append("$10: 0  ");
-                }
-
-                if (num >= 5) {
-                    stringBuilder.append("$5: 1  ");
-                    counter5++;
-                    num -= 5;
-                } else {
-                    stringBuilder.append("$5: 0  ");
-                }
-                stringBuilder.append("$1: ").append(num).append("\n");
-                counter1 += num;
-            }
-
-            outputField.setText(stringBuilder.toString());
-            counterField.setText("$50: " + counter50 + "\n" + "$20: " + counter20 + "\n" + "$10: " + counter10 + "\n"
-                    + "$5: " + counter5 + "\n" + "$1: " + counter1 + "\n" + "Итого $: " + (counter50 * 50 + counter20 * 20 +
-                    counter10 * 10 + counter5 * 5 + counter1));
+            thread.start();
 
         } catch (RuntimeException ex) {
             outputField.setText(ex.toString());
             counterField.clear();
+            testResultField.setStyle("");
+            actualField.clear();
+            expectedField.clear();
         }
     }
 
@@ -108,6 +151,9 @@ public class HelloController {
         inputField.clear();
         outputField.clear();
         counterField.clear();
+        testResultField.setStyle("");
+        actualField.clear();
+        expectedField.clear();
         counter50 = 0;
         counter20 = 0;
         counter10 = 0;
